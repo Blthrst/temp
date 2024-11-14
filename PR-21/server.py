3eef320400aec134e6203ee55d717a617b9055e2
Blthrst <135426305+Blthrst@users.noreply.github.com>
2024-11-14 09:51:40 +0000

    changed server code

--- PR-21/server.py
+++ PR-21/server.py
@@ -3,14 +3,12 @@ from http.server import HTTPServer
 
 def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
     server_address=('', 8000)
-
-    class MyHandler(handler_class):
-     def do_GET(self):
-        if self.path == '/ping':
-         self.send_response(200, "PONG")
     
-    httpd = server_class(server_address, MyHandler)
+    httpd = server_class(server_address, handler_class)
+
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
         httpd.server_close()
+
+run()
